ba61ce21b60c61851816f27950ba1e2f8f97a9c5
Jake Donahue <jdonahue135@gmail.com>
2020-05-12 16:29:17 -0400

    change parameters for ship factory to take in a ship name, then base length off that

--- src/Ship.js
+++ src/Ship.js
@@ -1,31 +1,52 @@
-const Ship = (length) => {
-    const getLength = () => length;
-    let ship = []
-    const getShip = () => ship
-    for (let i = 0; i < length; i++) {
-        let shipSpace = {
-            'index': i,
-            'hitStatus': false
-        }
-        ship.push(shipSpace);
-    }
+const Ship = (name) => {
+  let length;
+  switch (name) {
+    case "Carrier":
+      length = 5;
+      break;
+    case "Battleship":
+      length = 4;
+      break;
+    case "Destroyer":
+      length = 3;
+      break;
+    case "Submarine":
+      length = 3;
+      break;
+    case "Patrol Boat":
+      length = 2;
+      break;
+    default:
+      break;
+  }
+  const getName = () => name;
+  const getLength = () => length;
+  let ship = [];
+  const getShip = () => ship;
+  for (let i = 0; i < length; i++) {
+    let shipSpace = {
+      index: i,
+      hitStatus: false,
+    };
+    ship.push(shipSpace);
+  }
 
-    const hit = (i) => {
-        ship[i].hitStatus = true;
-    }
+  const hit = (i) => {
+    ship[i].hitStatus = true;
+  };
 
-    const isSunk = () => {
-        let hitSpaces = 0;
-        ship.forEach(shipSpace => {
-            if (shipSpace.hitStatus === true) {
-                hitSpaces++;
-            }
-        })
-        let sunkStatus = (hitSpaces === length) ? true : false;
-        return sunkStatus;
-    }
+  const isSunk = () => {
+    let hitSpaces = 0;
+    ship.forEach((shipSpace) => {
+      if (shipSpace.hitStatus === true) {
+        hitSpaces++;
+      }
+    });
+    let sunkStatus = hitSpaces === length ? true : false;
+    return sunkStatus;
+  };
 
-    return { getLength, getShip, hit, isSunk }
+  return { getName, getLength, getShip, hit, isSunk };
 };
 
-module.exports =  Ship;
+module.exports = Ship;
--- src/Ship.test.js
+++ src/Ship.test.js
@@ -1,37 +1,37 @@
-const Ship = require('./Ship');
+const Ship = require("./Ship");
 
-test('get Ship length', () => {    
-    const mockShip = Ship(3);
+test("get Ship length", () => {
+  const mockShip = Ship("Submarine");
 
-    expect(mockShip.getLength()).toEqual(3);
+  expect(mockShip.getLength()).toEqual(3);
 });
 
-test('get ship hit array', () => {
-    const mockShip = Ship(3);
+test("get ship hit array", () => {
+  const mockShip = Ship("Submarine");
 
-    expect(mockShip.getShip()).toEqual([
-        { index: 0, hitStatus: false },
-        { index: 1, hitStatus: false },
-        { index: 2, hitStatus: false }
-      ]);
+  expect(mockShip.getShip()).toEqual([
+    { index: 0, hitStatus: false },
+    { index: 1, hitStatus: false },
+    { index: 2, hitStatus: false },
+  ]);
 });
 
-test('ship can be hit', () => {
-    const mockShip = Ship(3);
-    mockShip.hit(1);
-    
-    expect(mockShip.getShip()).toEqual([
-        { index: 0, hitStatus: false },
-        { index: 1, hitStatus: true },
-        { index: 2, hitStatus: false }
-      ]);
+test("ship can be hit", () => {
+  const mockShip = Ship("Submarine");
+  mockShip.hit(1);
+
+  expect(mockShip.getShip()).toEqual([
+    { index: 0, hitStatus: false },
+    { index: 1, hitStatus: true },
+    { index: 2, hitStatus: false },
+  ]);
 });
 
-test('check if ship is sunk', () => {
-    const mockShip = Ship(3);
-    mockShip.hit(0);
-    mockShip.hit(1);
-    mockShip.hit(2);
+test("check if ship is sunk", () => {
+  const mockShip = Ship("Submarine");
+  mockShip.hit(0);
+  mockShip.hit(1);
+  mockShip.hit(2);
 
-    expect(mockShip.isSunk()).toEqual(true);
+  expect(mockShip.isSunk()).toEqual(true);
 });
