04f269819ea50e220358dba700a768a9c620b464
Jake Donahue <jdonahue135@gmail.com>
2020-05-13 17:21:38 -0400

    add all methods to Gameboard factory and test

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -2,21 +2,9 @@ const Ship = require("./Ship");
 
 const Gameboard = () => {
   let grid = [];
-  const getGrid = () => grid;
-  //Gameboard needs to track the coordinates of it's grid
-  //first for loop is based on charcodes A (65) - J (74)
-  for (let i = 65; i < 75; i++) {
-    for (let n = 1; n < 11; n++) {
-      let gridItem = {
-        index: String.fromCharCode(i) + n,
-        hitStatus: false,
-        ship: null,
-      };
-      grid.push(gridItem);
-    }
-  }
-  //and weather all of their ships are sunk or not
-  let ships = [
+
+  // Tracks ships
+  const ships = [
     Ship("Carrier"),
     Ship("Battleship"),
     Ship("Destroyer"),
@@ -24,8 +12,78 @@ const Gameboard = () => {
     Ship("Patrol Boat"),
   ];
 
-  //and needs to be able to absord hits
-  return { getGrid };
+  const getShips = () => ships;
+
+  // Tracks the coordinates of it's grid
+  let index = 0;
+  for (let i = 65; i < 75; i++) {
+    //'i' is based on charcodes A (65) - J (74)
+    for (let n = 1; n < 11; n++) {
+      //this gives us coordinates like 'A5' and 'D1'
+      let gridItem = {
+        //starts with index 0
+        index: index,
+        coordinates: String.fromCharCode(i) + n,
+        hitStatus: false,
+        shipName: null,
+        shipIndex: null,
+      };
+      grid.push(gridItem);
+      index++;
+    }
+  }
+
+  // Dummy placements for ships during development
+  //maybe simplify by combining orientation and indexes into an array of indexes?
+  const placeShip = (shipName, orientation, firstIndex) => {
+    const ship = ships.find((ship) => ship.getName() === shipName);
+
+    // Increments index correctly for horizontal or vertical orientation
+    let increment = orientation === "horizontal" ? 1 : 10;
+
+    //place ship on grid items
+    for (let i = 0; i < ship.getLength(); i++) {
+      // find gridItem
+      let gridItem = grid.find(
+        (theItem) => theItem.index === firstIndex + i * increment
+      );
+
+      // assign ship to gridName
+      gridItem.shipName = shipName;
+      gridItem.shipIndex = i;
+    }
+  };
+
+  // Tracks attacks
+  const receiveAttack = (coordinates) => {
+    let gridSpace = grid.find((space) => space.coordinates === coordinates);
+    gridSpace.hitStatus = true;
+
+    if (gridSpace.shipIndex !== null) {
+      // ship was hit()!
+      let theShip = ships.find((ship) => gridSpace.shipName === ship.getName());
+      theShip.hit(gridSpace.shipIndex);
+
+      //is ship sunk?
+      if (theShip.isSunk() === true) {
+        console.log("You sunk my " + theShip.getName());
+      }
+    }
+
+    //This won't need to return a value post-testing
+    return gridSpace;
+  };
+
+  const areAllSunk = () => {
+    for (let i = 0; i < ships.length; i++) {
+      if (ships[i].isSunk() === false) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  return { receiveAttack, placeShip, getShips, areAllSunk };
 };
 
 module.exports = Gameboard;
--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -1,7 +1,101 @@
 const Gameboard = require("./Gameboard");
 
-test("gameboard has a grid", () => {
+test("gameboard tracks location of hits on grid", () => {
   const gameboard = Gameboard();
 
-  expect(gameboard.getGrid()).toEqual([]);
+  expect(gameboard.receiveAttack("B4")).toEqual({
+    coordinates: "B4",
+    hitStatus: true,
+    index: 13,
+    shipIndex: null,
+    shipName: null,
+  });
+});
+
+test("gameboard tracks location of horizontal boat on grid", () => {
+  const gameboard = Gameboard();
+
+  gameboard.placeShip("Carrier", "horizontal", 13);
+
+  expect(gameboard.receiveAttack("B4")).toEqual({
+    coordinates: "B4",
+    hitStatus: true,
+    index: 13,
+    shipIndex: 0,
+    shipName: "Carrier",
+  });
+  expect(gameboard.receiveAttack("B5")).toEqual({
+    coordinates: "B5",
+    hitStatus: true,
+    index: 14,
+    shipIndex: 1,
+    shipName: "Carrier",
+  });
+});
+
+test("gameboard tracks location of vertical boat on grid", () => {
+  const gameboard = Gameboard();
+
+  gameboard.placeShip("Carrier", "vertical", 13);
+
+  expect(gameboard.receiveAttack("B4")).toEqual({
+    coordinates: "B4",
+    hitStatus: true,
+    index: 13,
+    shipIndex: 0,
+    shipName: "Carrier",
+  });
+  expect(gameboard.receiveAttack("C4")).toEqual({
+    coordinates: "C4",
+    hitStatus: true,
+    index: 23,
+    shipIndex: 1,
+    shipName: "Carrier",
+  });
+});
+
+test("gameboard tracks when ship is sunk", () => {
+  const gameboard = Gameboard();
+
+  gameboard.placeShip("Patrol Boat", "vertical", 13);
+
+  gameboard.receiveAttack("C4");
+
+  expect(gameboard.receiveAttack("B4")).toEqual({
+    coordinates: "B4",
+    hitStatus: true,
+    index: 13,
+    shipIndex: 0,
+    shipName: "Patrol Boat",
+  });
+});
+
+test("gameboard reports when all ships are sunk", () => {
+  const gameboard = Gameboard();
+
+  gameboard.placeShip("Patrol Boat", "horizontal", 0);
+  gameboard.placeShip("Submarine", "horizontal", 10);
+  gameboard.placeShip("Destroyer", "horizontal", 20);
+  gameboard.placeShip("Battleship", "horizontal", 30);
+  gameboard.placeShip("Carrier", "horizontal", 40);
+
+  gameboard.receiveAttack("A1");
+  gameboard.receiveAttack("A2");
+  gameboard.receiveAttack("B1");
+  gameboard.receiveAttack("B2");
+  gameboard.receiveAttack("B3");
+  gameboard.receiveAttack("C1");
+  gameboard.receiveAttack("C2");
+  gameboard.receiveAttack("C3");
+  gameboard.receiveAttack("D1");
+  gameboard.receiveAttack("D2");
+  gameboard.receiveAttack("D3");
+  gameboard.receiveAttack("D4");
+  gameboard.receiveAttack("E1");
+  gameboard.receiveAttack("E2");
+  gameboard.receiveAttack("E3");
+  gameboard.receiveAttack("E4");
+  gameboard.receiveAttack("E5");
+
+  expect(gameboard.areAllSunk()).toEqual(true);
 });
