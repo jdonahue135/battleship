846a39c4931aceb7bdd76abd0981f067a1d73ae1
Jake Donahue <jdonahue135@gmail.com>
2020-05-15 18:09:44 -0400

    remove computerPlay function to simplify code

--- src/Player.js
+++ src/Player.js
@@ -1,16 +1,18 @@
 const Player = (name) => {
   const getName = () => name;
-  const attack = (coordinateIndex, gameboard) => {
-    return gameboard.receiveAttack(coordinateIndex);
-  };
-
-  const computerAttack = (gameboard) => {
+  const attack = (gameboard, coordinateIndex) => {
+    if (coordinateIndex) {
+      console.log(coordinateIndex);
+      return gameboard.receiveAttack(coordinateIndex);
+    }
     //get number from 0-99
-    let coordinateIndex = Math.floor(Math.random() * 100);
+    coordinateIndex = Math.floor(Math.random() * 100);
+    console.log(coordinateIndex);
+
     return gameboard.receiveAttack(coordinateIndex);
   };
 
-  return { getName, attack, computerAttack };
+  return { getName, attack };
 };
 
 module.exports = Player;
