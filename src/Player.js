3cbc9940fb7c110db1cf1a40309b256d0a459d1e
Jake Donahue <jdonahue135@gmail.com>
2020-05-18 16:38:46 -0400

    simplify attack function by removing computerPlay function

--- src/Player.js
+++ src/Player.js
@@ -1,15 +1,8 @@
 const Player = (name) => {
   const getName = () => name;
   const attack = (gameboard, coordinateIndex) => {
-    if (coordinateIndex) {
-      console.log(coordinateIndex);
-      return gameboard.receiveAttack(coordinateIndex);
-    }
-    //get number from 0-99
-    coordinateIndex = Math.floor(Math.random() * 100);
-    console.log(coordinateIndex);
-
-    return gameboard.receiveAttack(coordinateIndex);
+    const result = gameboard.receiveAttack(coordinateIndex);
+    return result;
   };
 
   return { getName, attack };
