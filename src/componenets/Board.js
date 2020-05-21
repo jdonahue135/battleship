5503ab8456027111d71b0820ba5a8b659f407fae
Jake Donahue <jdonahue135@gmail.com>
2020-05-21 14:32:29 -0400

    remove hover function

--- src/componenets/Board.js
+++ src/componenets/Board.js
@@ -4,29 +4,14 @@ import GridSpace from "./GridSpace";
 const Board = (props) => {
   let containerId = props.onClick ? "computer-container" : "player-container";
   const grid = props.board.map((gridSpace) => {
-    const hoverSpaces = props.hoverSpaces;
-    if (hoverSpaces && hoverSpaces.includes(gridSpace.index)) {
-      return (
-        <GridSpace
-          hover={true}
-          onHover={props.onHover}
-          key={gridSpace.coordinates}
-          gridSpace={gridSpace}
-          onClick={props.onClick}
-          onDrop={props.onDrop}
-        />
-      );
-    } else {
-      return (
-        <GridSpace
-          onHover={props.onHover}
-          key={gridSpace.coordinates}
-          gridSpace={gridSpace}
-          onClick={props.onClick}
-          onDrop={props.onDrop}
-        />
-      );
-    }
+    return (
+      <GridSpace
+        key={gridSpace.coordinates}
+        gridSpace={gridSpace}
+        onClick={props.onClick}
+        onDrop={props.onDrop}
+      />
+    );
   });
   return (
     <div id={containerId} className="gameboard-container">
